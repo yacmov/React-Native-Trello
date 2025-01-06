174bf29cf2bcd2d57bcf70a2a0f1ac063fe62d60
Cedric Ko <yacmov@gmail.com>
2025-01-07 00:42:26 +0800

    feat: new task input

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, TextInput, Button } from "react-native";
 import React, { useState } from "react";
 import TaskListItem from "./TaskListItem";
 
@@ -8,9 +8,16 @@ const TaskList = () => {
     { description: "Second tasks" },
     { description: "Third tasks" },
   ]);
+  const [newTask, setNewTask] = useState("");
+
+  const createTask = () => {
+    setTasks([...tasks, { description: newTask }]);
+    setNewTask("");
+  };
+
   return (
-    <View className="p-[10] bg-[#101112] rounded-lg">
-      <Text className="text-white font-bold text-xl mx-[10] mb-4">TODO</Text>
+    <View className="p-[10] bg-[#101112] rounded-lg gap-[10]">
+      <Text className="text-white font-bold text-xl mx-[10]">TODO</Text>
 
       {/* The List of Tasks */}
       <FlatList
@@ -20,6 +27,14 @@ const TaskList = () => {
       />
 
       {/* New Task Input */}
+      <TextInput
+        value={newTask}
+        onChangeText={setNewTask}
+        placeholder="New Task"
+        placeholderTextColor={"gray"}
+        className="text-white font-bold bg-[#1d2125] rounded-md p-[10]"
+      />
+      <Button title="Add task" onPress={createTask} />
     </View>
   );
 };
