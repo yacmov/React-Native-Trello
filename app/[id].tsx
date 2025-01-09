a7e363a94b5d1dc9b2ea404ca8dbdefeef7b4292
Cedric Ko <yacmov@gmail.com>
2025-01-09 15:21:44 +0800

    feat: update task

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,22 +1,39 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput } from "react-native";
+import React, { useState } from "react";
 import { useLocalSearchParams } from "expo-router";
-import { useObject } from "@realm/react";
+import { useObject, useRealm } from "@realm/react";
 import { Task } from "@/models/Task";
 import { BSON } from "realm";
 
 const TaskDetails = () => {
   const { id } = useLocalSearchParams();
-
   const task = useObject<Task>(Task, new BSON.ObjectID(id as string));
+  const [updatedDescription, setUpdatedDescription] = useState(
+    task?.description
+  );
 
+  const realm = useRealm();
+
+  const updateDescription = () => {
+    if (!task) {
+      return;
+    }
+    realm.write(() => {
+      task.description = updatedDescription as string;
+    });
+  };
   if (!task) {
     return <Text>Not found</Text>;
   }
 
   return (
     <View className="p-4">
-      <Text className="text-white text-2xl">{task.description}</Text>
+      <TextInput
+        value={updatedDescription}
+        onChangeText={setUpdatedDescription}
+        onEndEditing={updateDescription}
+        className="text-white text-2xl"
+      />
     </View>
   );
 };
